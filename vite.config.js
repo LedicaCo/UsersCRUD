0b91a1dc440d11723dbcd4bbe134acf70df741ee
LedicaCo <webdesign@ledica.co>
2023-06-13 22:25:27 -0500

    deploy UsersCRUD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://ledicaco.github.io/UsersCRUD/',
   server: {
     host: true,
     open: true
