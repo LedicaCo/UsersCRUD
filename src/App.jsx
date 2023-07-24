3e757388db858b3fc6fb8037051259a4111abbda
LedicaCo <webdesign@ledica.co>
2023-07-23 22:16:08 -0500

    corrección baseUrl

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [updateInfo, setUpdateInfo] = useState();
 
   /* const baseUrl = "https://users-crud.academlo.tech/"; */
-  const baseUrl = "https://users-crud-api-fa1v.onrender.com/";
+  const baseUrl = "https://users-crud-api-fa1v.onrender.com/api/v1/users/";
   const [users, getAllUser, createNewUser, deleteUserById, updateUserById] =
     useFetch(baseUrl);
 
